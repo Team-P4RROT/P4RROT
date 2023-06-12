31f36851775eae44addb4f36151c199c2926540e
Jonas Köppeler <j.koeppeler@tu-berlin.de>
2023-06-12 09:16:43 +0200

    PSA eBPF: Core Test: AssignConst
    Assign constant values for different data types and send the packet back to sender.

---
+++ tests/psa_ebpf/assign_const/run_test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+TARGET_DIR="./template"
+
+cp -r ../../../templates/p4_psa_ebpf_template/* ${TARGET_DIR}
+
+mkdir -p ./template
+cp -r ../../../templates/p4_psa_ebpf_template/* ./template
+python3 codegen.py
+
+p4c-ebpf --arch psa -o main.c $TARGET_DIR/main.p4
+clang -O2 -g -c -DBTF -emit-llvm -o main.bc main.c
+llc -march=bpf -mcpu=generic -filetype=obj -o main.o main.bc
